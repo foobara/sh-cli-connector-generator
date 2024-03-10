b91a5fdad2061893d6cb56e35a4d3bd338990ec0
Miles Georgi <azimux@gmail.com>
2024-03-09 20:06:53 -0800

    Remove some unnecessary overrides

--- src/generators/sh_cli_connector_generator.rb
+++ src/generators/sh_cli_connector_generator.rb
@@ -25,17 +25,6 @@ module Foobara
           def templates_dir
             "#{__dir__}/../../templates"
           end
-
-          # TODO: promote this up to base project
-          def ==(other)
-            # :nocov:
-            self.class == other.class && sh_cli_connector_config == other.sh_cli_connector_config
-            # :nocov:
-          end
-
-          def hash
-            sh_cli_connector_config.hash
-          end
         end
       end
     end
