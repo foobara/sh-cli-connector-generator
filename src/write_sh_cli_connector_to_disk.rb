2e516b829608fa92b060929546c9835129e58e24
Miles Georgi <azimux@gmail.com>
2024-03-11 18:52:03 -0700

    Add --no-server to rubocop call

--- src/write_sh_cli_connector_to_disk.rb
+++ src/write_sh_cli_connector_to_disk.rb
@@ -46,7 +46,7 @@ module Foobara
         end
 
         def rubocop_autocorrect
-          Open3.popen3("bundle exec rubocop -A") do |_stdin, _stdout, stderr, wait_thr|
+          Open3.popen3("bundle exec rubocop --no-server -A") do |_stdin, _stdout, stderr, wait_thr|
             exit_status = wait_thr.value
             unless exit_status.success?
               warn "WARNING: could not rubocop -A. #{stderr.read}"
