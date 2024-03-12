b52d5419d5835f639add3ef1680e115a88433d63
Miles Georgi <azimux@gmail.com>
2024-03-11 17:15:11 -0700

    Don't add to Gemfile if gem is already there

--- src/generators/gemfile_generator.rb
+++ src/generators/gemfile_generator.rb
@@ -5,6 +5,10 @@ module Foobara
         # Kind of tricky... for the first time we will be loading an existing file in the working directory
         # and modifying it.
         class GemfileGenerator < ShCliConnectorGenerator
+          def applicable?
+            gemfile_contents !~ /^\s*gem\s*["']foobara-sh-cli-connector\b/
+          end
+
           def template_path
             "Gemfile"
           end
@@ -14,9 +18,7 @@ module Foobara
           end
 
           def generate(_elements_to_generate)
-            contents = File.read(template_path)
-
-            match = contents.match(/^gem /)
+            match = gemfile_contents.match(/^gem /)
 
             if match
               new_entry = 'gem "foobara-sh-cli-connector", github: "foobara/sh-cli-connector"'
@@ -27,6 +29,10 @@ module Foobara
               raise "Not sure how to inject sh_cli_connector into the Gemfile"
               # :nocov:
             end
+          end
+
+          def gemfile_contents
+            File.read(template_path)
           end
         end
       end
